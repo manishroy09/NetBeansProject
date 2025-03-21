3425bb7e8ca85450c4d799705896df1cad51756f
manishroy09 <manishroy2163@gmail.com>
2025-03-21 20:44:34 +0530

    21st march 2025

---
+++ DemoHibernate/src/java/com/manish/operations/FindAll.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.manish.operations;
+
+import com.manish.bean.Student;
+import com.manish.utility.HibernateUtil;
+import java.util.ArrayList;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author manis
+ */
+public class FindAll {
+    public static void main(String[] args) {
+         //step1: create object of session factory 
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        //step2: create object of session 
+        Session session = sf.openSession();
+        //step3: create object of student Query interface 
+        Query q = session.createQuery("from Student s");
+        ArrayList<Student> all = ( ArrayList<Student>)q.list();
+        //Step4: Traverse data using for each 
+        for(Student sb: all){
+            System.out.println(""+sb.getEnroll()+"\t"+sb.getName()+"\t"+sb.getC());
+        }
+      
+    }
+}
