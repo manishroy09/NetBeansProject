086805cfecd84e56261e1729970b012b1c62bb05
manishroy09 <manishroy2163@gmail.com>
2025-03-22 20:42:01 +0530

    22nd march 2025

---
+++ DemoHibernate/src/java/com/manish/bean/Product.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.manish.bean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author manis
+ */
+@Entity
+@Table(name="PRODUCT_TBL")
+public class Product {
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    @Column(name="Product-id")
+    private int pid;
+    @Column(name="Product_Name")
+    private String pname;
+    private float  price ;
+
+    public Product() {
+    }
+
+    public Product(int pid, String pname, float price) {
+        this.pid = pid;
+        this.pname = pname;
+        this.price = price;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    
+    
+}
--- DemoHibernate/src/java/com/manish/operations/AddProduct.java
+++ DemoHibernate/src/java/com/manish/operations/AddProduct.java
@@ -5,7 +5,7 @@
  */
 package com.manish.operations;
 
-import com.manish.bean.Student;
+import com.manish.bean.Product;
 import com.manish.utility.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -15,36 +15,29 @@ import org.hibernate.Transaction;
  *
  * @author manis
  */
-public class InsertData {
+public class AddProduct {
     public static void main(String[] args) {
-        //step1: create object of session factory 
+         //step1: create object of session factory 
         SessionFactory sf = HibernateUtil.getSessionFactory();
         //step2: create object of session 
         Session session = sf.openSession();
-        //step3: create object of student bean class
-        Student sb = new Student();
-        //step4: set all data into beans
-        sb.setC(88);
-        sb.setE(99);
-       sb.setEnroll("0111CS221031");
-        sb.setH(88);
-        sb.setM(89);
-        sb.setName("Aman Yadav");
-        sb.setP(79);
-        sb.setSid(31);
-        int total = sb.getP()+sb.getC()+sb.getM()+sb.getH()+sb.getE();
-         float per = total/5.0f;
-         sb.setTotal(total);
-         sb.setPer(per);
-         //step5: calll save method 
-         session.save(sb);
+        //step3: create object of  product  class
+        Product p = new Product();
+        // step4: set data into bean 
+       p.setPid(101);
+       p.setPname("mobile");
+       p.setPrice(12000.56f);
+       
+       //step5: calll save method 
+         session.save(p);
          //step6: create an object of transaction 
          Transaction tx = session.beginTransaction();
          //Step7: call commit method 
          tx.commit();
+         System.out.println("Product Add Success");
          //Stpe8: close
          session.close();
          sf.close();
     }
+       
 }
-
